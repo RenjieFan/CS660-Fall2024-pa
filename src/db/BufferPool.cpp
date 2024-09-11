38d8c3ed18e41f0c84601d798ea6814d6dc12921
RenjieFan <fanrenjie0812@163.com>
2024-09-12 05:23:09 +0800

    Update BufferPool.cpp

--- src/db/BufferPool.cpp
+++ src/db/BufferPool.cpp
@@ -23,22 +23,45 @@ Page &BufferPool::getPage(const PageId &pid) {
 }
 
 void BufferPool::markDirty(const PageId &pid) {
+   if (!contains(pid)) {
+            throw std::runtime_error("Page not in buffer pool");
+   }
+        page_map[pid]->is_dirty = true;
   // TODO pa1: Mark the page as dirty. Note that the page must already be in the buffer pool
 }
 
 bool BufferPool::isDirty(const PageId &pid) const {
+   if (!contains(pid)) {
+            throw std::runtime_error("Page not in buffer pool");
+        }
+        return page_map.at(pid)->is_dirty;
   // TODO pa1: Return whether the page is dirty. Note that the page must already be in the buffer pool
 }
 
 bool BufferPool::contains(const PageId &pid) const {
+   return page_map.find(pid) != page_map.end();
   // TODO pa1: Return whether the page is in the buffer pool
 }
 
 void BufferPool::discardPage(const PageId &pid) {
+  if (!contains(pid)) {
+            return;
+        }
+        auto it = page_map[pid];
+        lru_list.erase(it);
+        page_map.erase(pid);
   // TODO pa1: Discard the page from the buffer pool. Note that the page must already be in the buffer pool
 }
 
 void BufferPool::flushPage(const PageId &pid) {
+  if (!contains(pid)) {
+            throw std::runtime_error("Page not in buffer pool");
+        }
+        auto it = page_map[pid];
+        if (it->is_dirty) {
+            Database::getFileManager().writePage(it->page);
+            it->is_dirty = false;
+        }
   // TODO pa1: Flush the page to disk. Note that the page must already be in the buffer pool
 }
 
